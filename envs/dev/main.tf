5bf5b1bd80d6fcfcb9ffa43ae92c6c37bfffea6a
avinash-ingle-sre <avinash.ingle.sre@gmail.com>
2025-05-24 17:09:40 +0530

    providr name issie

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,9 +1,13 @@
 provider "aws" {
+  alias  = "dev"
   region = "us-east-1"
 }
 
 module "vpc" {
-  source = "../../modules/vpc"
+  source     = "../../modules/vpc"
   cidr_block = "10.0.0.0/16"
-  env = "dev"
+  env        = "dev"
+  providers  = {
+    aws = aws.dev
+  }
 }
